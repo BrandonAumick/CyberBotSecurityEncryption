3a19bb10aa7cf230df5f99835a72a67fb9ee21df
Brendo <32585448+BrandonAumick@users.noreply.github.com>
2022-04-28 09:34:44 -0400

    Added letter location finding script

---
+++ find_letter_in_alphabet.py
@@ -0,0 +1,11 @@
+from microbit import *
+ 
+sleep(1000)
+ 
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ 
+letter = "M"
+index = alpha.find(letter)
+ 
+print("letter =", letter, "index =", index)
+print()
